3e272aa690cba98ad6796adf8ef54b2257ab099c
pizzmile <giacomo.pizza99@gmail.com>
2022-08-02 14:07:36 +0200

    server v4

--- simulation/server/utils/db_manager.py
+++ simulation/server/utils/db_manager.py
@@ -19,14 +19,14 @@ class DBManager:
         logger.debug("Created connector to DB")
 
     def get_device_id(self, dev_name: str):
-        sql = "SELECT idDevice FROM SS_DB.device WHERE name = %s"
+        sql = "SELECT idDevice FROM SS_DB.devices WHERE name = %s"
         var = (dev_name,)
         self.__cursor.execute(sql, var)
         result = self.__cursor.fetchone()
         return result[0]
 
     def insert_processed_work(self, timestamp: str, duration: str, defective: str, success: str, id_device: str):
-        sql = "INSERT INTO SS_DB.processedWork (timestamp, duration, defective, success, idDevice) " \
+        sql = "INSERT INTO SS_DB.works (timestamp, duration, defective, success, idDevice) " \
               "VALUES (%s, %s, %s, %s, %s)"
         var = (timestamp, duration, defective, success, id_device,)
         self.__cursor.execute(sql, var)
