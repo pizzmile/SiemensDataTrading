d887bbca2f48fdad63c1fcd47bf68880cfbc9f53
pizzmile <giacomo.pizza99@gmail.com>
2022-08-03 14:30:34 +0200

    udp_socket implementation completed

--- simulation/server/udp_server.py
+++ simulation/server/udp_server.py
@@ -7,10 +7,22 @@ from datetime import datetime
 
 from utils import DBManager
 
+
+def parse_packet(packet: bytes):
+    parsed_packet = {}
+
+    packet_str = packet.decode("utf-8")
+    packet_fields_raw = packet_str.split("&")
+    for field in packet_fields_raw:
+        packet_field = field.split(":")
+        parsed_packet[packet_field[0]] = packet_field[1]
+
+    return parsed_packet
+
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename="log.log", level=logging.DEBUG,
                     format='[%(asctime)s] %(levelname)s - %(message)s', datefmt='%H:%M:%S')
-
 
 # Load configuration from file
 with open('./config.json', 'r') as f:
@@ -33,3 +45,26 @@ if __name__ == '__main__':
         message = byte_address_pair[0]
         address = byte_address_pair[1]
         print(f"Message from {address}: {message}")
+
+        if message:
+            # parse data
+            parsed_data = parse_packet(message)
+            dev_id = dbmanager.get_device_id(parsed_data["devName"])
+
+            # log
+            logger.debug(f"Received message: {parsed_data}")
+            print(parsed_data)
+
+            # get current timestamp
+            timestamp_format = '%Y-%m-%d %H:%M:%S'
+            timestamp = datetime.now().strftime(timestamp_format)
+            # write to db
+            dbmanager.insert_processed_work(
+                timestamp,
+                str(dev_id),
+                parsed_data["current"],
+                parsed_data["voltage"],
+                parsed_data["activePower"],
+                parsed_data["reactivePower"],
+                parsed_data["apparentPower"]
+            )
